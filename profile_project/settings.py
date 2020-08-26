1480ec872da7115bbe97ae39b200c6054ac120a7
MJ Moosavizade <mjmoosavizade@gmail.com>
2020-08-26 17:20:51 +0430

    Added database

--- profile_project/settings.py
+++ profile_project/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'profile_api',
-    
 ]
 
 MIDDLEWARE = [
@@ -122,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profile_api.UserProfile'
